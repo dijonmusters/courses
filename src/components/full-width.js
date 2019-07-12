ffcbbcf531d4b6a7490da4d09d598358c35066b7
Jon Meyers <jon.meyers@rea-group.com>
2019-07-12 21:33:15 +1000

    WIP: Implement full width component

---
+++ src/components/full-width.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Red = styled.div`
+  background-color: transparent;
+  width: 100vw;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+`;
+
+const FullWidth = props => {
+  return <Red>{props.children}</Red>
+}
+
+export default FullWidth;

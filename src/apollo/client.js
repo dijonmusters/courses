f489475d61ba340a2f39939d56bf6f7fe586864b
Jon Meyers <jon.meyers@rea-group.com>
2019-09-07 11:49:32 +1000

    Apollo link working

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,18 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
+import netlifyIdentity, { currentUser as getCurrentUser } from 'netlify-identity-widget';
 
 const isClient = typeof window !== 'undefined';
+
+const getAuthHeaders = async () => {
+  netlifyIdentity.init();
+  const user = getCurrentUser && getCurrentUser();
+  if (user) {
+    const token = await user.jwt();
+    return { Authorization: `Bearer ${token}` };
+  }
+  return {};
+}
 
 const host = isClient
   ? window.location.origin
@@ -9,11 +20,32 @@ const host = isClient
 
 const uri = `${host}/.netlify/functions/graphql`;
 
+
+
 const getClient = async () => {
-  return new ApolloClient({
-    uri,
-    fetch,
+  const headers = await getAuthHeaders();
+
+  const authLink = new ApolloLink((operation, forward) => {
+    operation.setContext({ headers });
+    return forward(operation);
   });
+
+  const httpLink = new HttpLink({
+    uri,
+    headers,
+    fetch
+  });
+
+  return new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  });
+  // NEED TO GET THIS
+
+  // return new ApolloClient({
+  //   uri,
+  //   fetch
+  // });
 }
 
 export { getClient }
